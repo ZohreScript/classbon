35d87d7e7da05b231692c8095d7a433f4ae17054
Abbas Sepahvand <abbassep@outlook.com>
2024-11-09 17:45:30 +0330

    complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Colors } from "@/app/_components/colors";
 import Image from "next/image";
+import { Button } from "./_components/button/button";
 export default function Home() {
   return (
     <>
@@ -17,6 +18,14 @@ export default function Home() {
               کلاسبن می‌تونی بدون محدودیت به قله‌های بالاتر صعود کنی. ما همیشه
               هواتو داریم.
             </p>
+            <div className="mt-5 flex gap-4">
+              <Button variant="primary" size="large">
+                دوره‌های ری‌اکت و نکست
+              </Button>
+              <Button variant="neutral" size="large">
+                مشاوره برنامه‌نویسی
+              </Button>
+            </div>
             <Image
               src="/images/frameworks.png"
               className="grayscale mt-4 opacity-70 m-auto xl:m-0"
