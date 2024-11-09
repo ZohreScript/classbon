708f6b7630a380c9303aa792e40b6a2e1885d6fd
Abbas Sepahvand <abbassep@outlook.com>
2024-11-09 17:45:20 +0330

    update

---
+++ src/app/account/student/page.tsx
@@ -0,0 +1,4 @@
+export default function Page() {
+    throw 'error';
+    return <h1>This is student page</h1>
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,17 @@
-import './globals.css'
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body >{children}</body>
+      <body className="flex flex-col min-h-screen">
+        <header className="bg-gray-200 h-10 flex justify-center items-center"> HEADER</header>
+        <div className="flex-1">{children}</div>
+        <footer className="bg-gray-200 h-10 flex justify-center items-center"> FOOTER</footer>
+      </body>
     </html>
-  )
+  );
 }
