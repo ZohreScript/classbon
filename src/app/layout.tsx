859ec3248ae515b17ed12bffdd9a4ec0b63f482a
ZohreScript <z.omidi74@gmail.com>
2024-11-09 17:45:32 +0330

    button component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,41 +12,7 @@ const figtree = Figtree({
   weight: ["300", "400", "500", "600", "700", "700", "800", "900"],
 });
 
-const yekanbakh = localFont({
-  src: [
-    {
-      path: "../../public/fonts/yekanbakh/YekanBakhFaNum-Thin.woff2",
-      weight: "100",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/yekanbakh/YekanBakhFaNum-Light.woff2",
-      weight: "300",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/yekanbakh/YekanBakhFaNum-Regular.woff2",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/yekanbakh/YekanBakhFaNum-SemiBold.woff2",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/yekanbakh/YekanBakhFaNum-Bold.woff2",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "../../public/fonts/yekanbakh/YekanBakhFaNum-Black.woff2",
-      weight: "900",
-      style: "normal",
-    },
-  ],
-  variable: "--font-yekanbakh",
-});
+
 
 export default function RootLayout({
   children,
@@ -56,7 +22,7 @@ export default function RootLayout({
   return (
     <html
       dir="rtl"
-      className={`${yekanbakh.variable} ${figtree.variable} dark`}
+      className={` ${figtree.variable} dark`}
     >
       <body className="min-h-screen grid grid-rows-[80px_1fr_auto] dark:bg-base-100 dark:text-base-content">
         <Header />
